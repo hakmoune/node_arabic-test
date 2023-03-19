5787312f3019fd8870eac75ea7e400917c4a4b7c
mehdi <mehdi.hakmoune@gmail.com>
2023-03-19 17:53:59 +0000

    Create Model Customer

---
+++ models/customers.js
@@ -0,0 +1,32 @@
+const moongose = require("mongoose");
+const joi = require("joi");
+
+const Customer = moongose.model(
+  "Customer",
+  new moongose.Schema({
+    name: { type: String, required: true, minlength: 3, maxlength: 255 },
+    mobile: { type: String, required: true, minlength: 3, maxlength: 15 },
+    isGold: { type: Boolean, default: false }
+  })
+);
+
+function joiValidateCustomer(customer) {
+  const schema = {
+    name: joi
+      .string()
+      .min(3)
+      .max(255)
+      .required(),
+    mobile: joi
+      .string()
+      .min(3)
+      .max(15)
+      .required(),
+    isGold: joi.boolean()
+  };
+
+  return joi.validate(customer, schema);
+}
+
+exports.Customer = Customer;
+exports.joiValidateCustomer = joiValidateCustomer;
